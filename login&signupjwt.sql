514cf75423701c8288916b1e920e1602df18c1f5
roopasri.r@techmango.net <roopasri.r@techmango.net>
2023-03-20 10:50:35 +0530

    initial  commit

---
+++ login&signupjwt.sql
@@ -0,0 +1,5 @@
+id,name,gender,email_id,phone_number,password,user_type,is_active,sso_type,login_at,login_count,created_at,updated_at
+1,Roopa,Female,roopa@gmail.com,9443269065,1702,NORMAL,1,NULL,NULL,0,"2023-01-09 10:48:15","2023-01-09 10:48:17"
+3,Kumar,Male,rk@gmail.com,8825972219,1708,NORMAL,1,NULL,NULL,0,"2023-01-09 10:55:07","2023-01-09 10:55:09"
+4,Nandha,Male,nandha@gmail.com,9894566321,1001,NORMAL,1,NULL,NULL,0,"2023-01-10 13:16:56","2023-01-10 13:16:58"
+5,Sudha,Female,sudha@gmail.com,9344456632,1002,NORMAL,1,NULL,NULL,0,"2023-03-17 10:19:25","2023-03-17 10:19:25"
